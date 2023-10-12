e208539eebcb55bea874fa6090947026b12ae5ec
Gooogr <gusarovga@ya.ru>
2023-10-12 14:52:45 +0300

    feat: adjust prediction range

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module github.com/Gooogr/meteo
 go 1.21.1
 
 require github.com/zsefvlol/timezonemapper v1.0.0 // direct
+
+require (
+	github.com/fatih/color v1.15.0 // direct
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -6,11 +6,45 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/fatih/color"
 	"github.com/zsefvlol/timezonemapper"
 )
 
 const apiURL = "https://api.open-meteo.com/v1/forecast"
+const maxRows = 12
+
+var weatherCodes = map[int]string{
+	0:  "Clear sky",
+	1:  "Mainly clear",
+	2:  "Partly cloudy",
+	3:  "Overcast",
+	45: "Fog",
+	48: "Rime fog",
+	51: "Light drizzle",
+	53: "Moderate drizzle",
+	55: "Dense drizzle",
+	56: "Light freezing drizzle",
+	57: "Dense freezing drizzle",
+	61: "Slight rain",
+	63: "Moderate rain",
+	65: "Heavy rain",
+	66: "Light freezing rain",
+	67: "Heavy freezing rain",
+	71: "Slight snow",
+	73: "Moderate snow",
+	75: "Heavy snow",
+	77: "Snow grains",
+	80: "Slight rain",
+	81: "Moderate rain",
+	82: "Heavy rain",
+	85: "Slight snow",
+	86: "Heavy snow",
+	95: "Thunderstorm",
+	96: "Thunderstorm with slight hail",
+	99: "Thunderstorm with heavy hail",
+}
 
 type WeatherData struct {
 	Latitude    float64         `json:"latitude"`
@@ -31,7 +65,7 @@ type HourlyData struct {
 	Time                     []string  `json:"time"` // TODO: convert directly to time.Time
 	Temperature2m            []float64 `json:"temperature_2m"`
 	PrecipitationProbability []float64 `json:"precipitation_probability"`
-	Weathercode              []float64 `json:"weathercode"`
+	Weathercode              []int     `json:"weathercode"`
 	Windspeed10m             []float64 `json:"windspeed_10m"`
 }
 
@@ -50,11 +84,10 @@ func createURL(apiURL string, loc Location) string {
 	return url
 }
 
-// "https://api.open-meteo.com/v1/forecast?latitude=55.7522&longitude=37.6156&hourly=temperature_2m,precipitation_probability,weathercode,windspeed_10m&timezone=Europe%2FMoscow&forecast_days=3"
-
 func main() {
 	// TODO: pass coords as arguments of CLI or get coords from device
 	var lat, lng float64 = 55.7522, 37.6156
+	timezone := timezonemapper.LatLngToTimezoneString(lat, lng) // TODO: refactor to avoid repeated code in createURL
 	res, err := http.Get(createURL(apiURL, Location{lat, lng}))
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
@@ -77,20 +110,75 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
-	// fmt.Println(weather)
 
-	for i, time := range weather.Hourly.Time {
+	currentTime := time.Now()
+	fmt.Println(currentTime)
+
+	i := 0
+	rowsCnt := 0
+	for i < len(weather.Hourly.Time) {
+		datetime := weather.Hourly.Time[i]
 		temperature := weather.Hourly.Temperature2m[i]
 		weathercode := weather.Hourly.Weathercode[i]
-		precipitation_probability := weather.Hourly.PrecipitationProbability[i]
+		precipitation := weather.Hourly.PrecipitationProbability[i]
 		windspeed := weather.Hourly.Windspeed10m[i]
-		fmt.Printf(
-			"%s - %.1f°C %.0f, %.0f  %.1fkm/h \n",
-			time,
-			temperature,
-			weathercode,
-			precipitation_probability,
-			windspeed,
+
+		// Decode weather conditions
+		weatherState, ok := weatherCodes[weathercode]
+		if !ok {
+			weatherState = ""
+		}
+
+		// Parse the datetime string
+		location, err := time.LoadLocation(timezone)
+		if err != nil {
+			fmt.Println("Error loading timezone:", err)
+			os.Exit(1)
+		}
+		datetimeParsed, err := time.ParseInLocation("2006-01-02T15:04", datetime, location)
+		if err != nil {
+			fmt.Println("Error parsing datetime:", err)
+			os.Exit(1)
+		}
+
+		// Skip historical data
+		if datetimeParsed.Before(currentTime) {
+			i += 1
+			continue
+		}
+		// Limit output size
+		if rowsCnt > maxRows {
+			break
+		}
+
+		// Extract and format the hour
+		hour := datetimeParsed.Hour()
+		formattedHour := fmt.Sprintf("%02d:00", hour)
+
+		// Define colors
+		temperatureColor := color.New(color.FgCyan).Sprintf("%.1f°C", temperature)
+		precipitationColor := color.New(color.FgGreen).Sprintf("%.0f%%", precipitation)
+		windspeedColor := color.New(color.FgBlue).Sprintf("%.1fkm/h", windspeed)
+
+		// Define column sizes
+		timeColumnSize := 6
+		temperatureColumnSize := 10
+		windspeedColumnSize := 10
+		precipitationColumnSize := 13
+		weatherColumnSize := 10
+
+		// Format and print the output
+		formatString := fmt.Sprintf(
+			"%%-%ds %%-%ds %%-%ds %%-%ds  %%-%ds\n",
+			timeColumnSize,
+			temperatureColumnSize,
+			windspeedColumnSize,
+			precipitationColumnSize,
+			weatherColumnSize,
 		)
+
+		fmt.Printf(formatString, formattedHour, temperatureColor, windspeedColor, precipitationColor, weatherState)
+		i += 1
+		rowsCnt += 1
 	}
 }
